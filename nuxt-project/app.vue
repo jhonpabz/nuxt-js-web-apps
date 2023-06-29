2b02cb19c228488b4801ef18e72c2c00225ea769
Jhon Cesar Pablo <jhon.cesar.pablo@skig.tech>
2023-06-29 18:50:37 +0800

    Routing.

---
+++ nuxt-project/app.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <ul>
+      <li>
+        <NuxtLink to="/">Home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/events">Events</NuxtLink>
+      </li>
+    </ul>
+  </header>
+
+  <NuxtPage />
+  <!--  <NuxtLayout>
+
+  </NuxtLayout> -->
+</template>
