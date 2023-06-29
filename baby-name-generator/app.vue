c116c2e3505610b12b12ec70c9918c6fa485e890
Jhon Cesar Pablo <jhon.cesar.pablo@skig.tech>
2023-06-29 17:12:18 +0800

    Initial commit.

---
+++ baby-name-generator/app.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Baby Name Generator</h1>
+    <p>Choose your baby name you want in the options below.</p>
+
+    <div class="options-container">
+      <div class="option-container">
+        <h4>1.) Choose a gender</h4>
+        <div class="option-buttons">
+          <button>Boy</button>
+          <button>Girl</button>
+          <button>Unisex</button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>2.) Choose the name popularity</h4>
+        <div class="option-buttons">
+          <button>Trendy</button>
+          <button>Unique</button>
+        </div>
+      </div>
+      <div class="option-container">
+        <h4>3.) Choose the name length</h4>
+        <div class="option-buttons">
+          <button>Long</button>
+          <button>Short</button>
+          <button>All</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
