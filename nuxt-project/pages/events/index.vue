1e3b35e977895f1539a119a51e5a418792a12bcc
Jhon Cesar Pablo <jhon.cesar.pablo@skig.tech>
2023-06-30 14:36:07 +0800

    Composables and Plugins.

--- nuxt-project/pages/events/index.vue
+++ nuxt-project/pages/events/index.vue
@@ -1,10 +1,13 @@
+<script setup>
+const { $hello } = useNuxtApp();
+const foo = useFoo();
+foo;
+console.log("$hello: ", $hello("JhonPabz"));
+</script>
+
 <template>
   <div class="bg-slate-900 h-screen text-white p-6">Events Page</div>
   <img src="@/assets/zxc.jpg" alt="Discover Nuxt 3" />
 </template>
-
-<script>
-export default {};
-</script>
 
 <style></style>
