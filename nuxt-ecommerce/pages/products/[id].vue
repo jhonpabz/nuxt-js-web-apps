2b589ce731e70a76b33670188a752a4cf64c7825
Jhon Cesar Pablo <jhon.cesar.pablo@skig.tech>
2023-06-30 17:39:13 +0800

    E commerce app.

---
+++ nuxt-ecommerce/pages/products/[id].vue
@@ -0,0 +1,10 @@
+<script setup>
+const route = useRoute();
+const paramsId = route.params.id;
+const nuxt = useNuxtApp();
+console.log(nuxt);
+</script>
+
+<template>
+  <div>Product id: {{ paramsId }}</div>
+</template>
