ee11be8b5d9947611ea91d6344167b242f429ce1
Jhon Cesar Pablo <jhon.cesar.pablo@skig.tech>
2023-06-30 17:04:57 +0800

    Server

---
+++ nuxt-project/pages/test/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+const counter = useCounter();
+const color = useColor();
+
+const handleClick = () => {
+  console.log(color.value);
+  color.value = "#fff";
+};
+</script>
+
+<template>
+  <div>
+    <div>Counter: {{ counter }}</div>
+    <button class="btn btn-success" @click="counter++">+</button>
+    <button class="btn btn-active" @click="counter--">-</button>
+  </div>
+  <div>
+    <div class="text-[#040404]">Color: {{ color }}</div>
+    <button class="btn btn-warning" @click="handleClick">Update Color</button>
+  </div>
+</template>
