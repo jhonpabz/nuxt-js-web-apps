f56c38cbb960de2c970e3974f786ade605d1b3ce
Jhon Cesar Pablo <jhon.cesar.pablo@skig.tech>
2023-07-01 10:43:33 +0800

    State management using reactive. Added sample methods.

--- nuxt-project/pages/index.vue
+++ nuxt-project/pages/index.vue
@@ -1,13 +1,32 @@
 <script setup>
+import { ref } from "vue";
 import { useCounter } from "@/store";
+
+const num = ref(null);
 </script>
 
 <template>
+  <!-- <ContentDoc /> -->
   <div class="bg-[#0F172A] h-screen text-white p-6">
-    <!-- <ContentDoc /> -->
     <div class="pb-4 text-3xl">
       {{ useCounter.count }}
     </div>
-    <button @click="useCounter.count++" class="btn">Increment</button>
+    <button @click="useCounter.increment()" class="btn btn-info mr-2">
+      Increment
+    </button>
+    <button @click="useCounter.incrementBy10()" class="btn btn-info">
+      Increment by 10
+    </button>
+
+    <div>
+      <input
+        type="number"
+        ref="num"
+        v-model="toAdd"
+        placeholder="Add number"
+        class="input w-full max-w-xs text-slate-900"
+      />
+      <button @click="useCounter.add(toAdd)" class="btn my-2 ml-1">Add</button>
+    </div>
   </div>
 </template>
--- nuxt-project/store/useCounter.js
+++ nuxt-project/store/useCounter.js
@@ -2,4 +2,15 @@ import { reactive } from "vue";
 
 export const useCounter = reactive({
   count: 0,
+  incrementBy10() {
+    this.count = this.count + 10;
+  },
+
+  increment() {
+    this.count++;
+  },
+
+  add(number) {
+    this.count = this.count + number;
+  },
 });
